07064790f8663f776187e3a0b5671c4ef96ea8b4
Erich Behrens <me@eb1.it>
2018-04-18 19:44:04 +0200

    Add github pages config

---
+++ webpack.config.pages.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+	entry: {
+		main: './src/pages/index.js',
+	},
+	output: {
+		path: path.join(__dirname, 'pages'),
+		filename: '[name].js',
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				loaders: ['css-hot-loader', MiniCssExtractPlugin.loader, 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]', 'postcss-loader'],
+			},
+			{
+				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
+			},
+		],
+	},
+
+	plugins: [
+		new webpack.NoEmitOnErrorsPlugin(),
+		new webpack.NamedModulesPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new HtmlWebpackPlugin({
+			template: './src/pages/template.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+			chunkFilename: '[name].css',
+		}),
+	],
+
+	devServer: {
+		contentBase: './public',
+		hot: true,
+		port: 8888,
+		host: 'localhost',
+	},
+
+	devtool: 'source-map',
+};
