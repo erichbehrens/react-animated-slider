2b935717415d7bfb9e2a48177ad6dc68b861e95c
Erich Behrens <me@eb1.it>
2018-05-03 22:43:52 +0200

    Code highlight

--- src/pages/SourceView.js
+++ src/pages/SourceView.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import hljs from 'highlight.js'; // eslint-disable-line import/no-extraneous-dependencies
+import 'highlight.js/styles/github-gist.css'; // eslint-disable-line import/no-extraneous-dependencies
 import styles from './SourceView.css';
 
 class SourceView extends React.Component {
@@ -6,13 +8,20 @@ class SourceView extends React.Component {
 
 	toggle = () => this.setState({ expanded: !this.state.expanded });
 
+	highlight = (ref) => {
+		hljs.configure({ languages: ['javascript'] });
+		hljs.highlightBlock(ref);
+	}
+
 	render() {
 		const { expanded } = this.state;
 		const { children } = this.props;
 		return (
 			<div className={styles.sourceView}>
 				<button className={styles.toggleButton} onClick={this.toggle}>{expanded ? 'Hide' : 'Show'} source</button>
-				<pre className={expanded ? styles.expanded : styles.collapsed}>{children}</pre>
+				<div className={`${styles.code} ${expanded ? styles.expanded : styles.collapsed}`}>
+					<pre ref={this.highlight}>{children}</pre>
+				</div>
 			</div>
 		);
 	}
